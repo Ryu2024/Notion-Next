6d45b0124ddec52b17fbe74681d01da13f79ff4d
Ryu2024 <liuchang15092@live.com>
2024-05-06 01:06:49 +0800

    blog

--- blog.config.js
+++ blog.config.js
@@ -122,7 +122,9 @@ const BLOG = {
     '/[prefix]/[slug]': 'LayoutSlug',
     '/[prefix]/[slug]/[...suffix]': 'LayoutSlug',
     '/signin': 'LayoutSignIn',
-    '/signup': 'LayoutSignUp'
+    '/signup': 'LayoutSignUp',
+	'/blog': 'LayoutPostList'
+	
   },
 
   CAN_COPY: process.env.NEXT_PUBLIC_CAN_COPY || false, // 是否允许复制页面内容 默认允许，如果设置为false、则全栈禁止复制内容。
