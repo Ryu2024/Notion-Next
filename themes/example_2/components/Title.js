b325eee77a02d79d5c179703e65e588c1bd4105b
Ryu2024 <117675219+Ryu2024@users.noreply.github.com>
2024-05-07 22:13:17 +0800

    hide author

--- themes/example_2/components/Title.js
+++ themes/example_2/components/Title.js
@@ -9,7 +9,7 @@ import { siteConfig } from '@/lib/config'
 export const Title = (props) => {
   const { post } = props
   const title = post?.title || siteConfig('TITLE')
-  const description = post?.description || siteConfig('AUTHOR')
+  const description = post?.description || //siteConfig('AUTHOR') # 不显示author
 
   return <div className="text-center px-6 py-12 mb-6 bg-gray-100 dark:bg-hexo-black-gray dark:border-hexo-black-gray border-b">
         <h1 className="text-xl md:text-4xl pb-4"><NotionIcon icon={post?.pageIcon} />{title}</h1>
