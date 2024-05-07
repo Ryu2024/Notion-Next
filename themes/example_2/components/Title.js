b857917e63c03af951d5155e4342767cd5861f80
Ryu2024 <117675219+Ryu2024@users.noreply.github.com>
2024-05-07 22:16:47 +0800

    Update Title.js

--- themes/example_2/components/Title.js
+++ themes/example_2/components/Title.js
@@ -9,8 +9,7 @@ import { siteConfig } from '@/lib/config'
 export const Title = (props) => {
   const { post } = props
   const title = post?.title || siteConfig('TITLE')
-  const description = post?.description || //siteConfig('AUTHOR') # 不显示author
-
+  //const description = post?.description || siteConfig('AUTHOR')
   return <div className="text-center px-6 py-6 mb-3 bg-gray-100 dark:bg-hexo-black-gray dark:border-hexo-black-gray border-b">
         <h1 className="text-xl md:text-4xl pb-4"><NotionIcon icon={post?.pageIcon} />{title}</h1>
         <p className="leading-loose text-gray-dark">
