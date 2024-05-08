455c18d4c77d6ff7b32880a257f85cdabbca9c85
Ryu2024 <117675219+Ryu2024@users.noreply.github.com>
2024-05-09 01:19:09 +0800

    Update BlogPostCard.js

--- themes/example_2/components/BlogPostCard.js
+++ themes/example_2/components/BlogPostCard.js
@@ -12,16 +12,18 @@ const BlogPostCard = ({ post }) => {
 
   return <article className={`${showPageCover ? 'flex md:flex-row flex-col-reverse' : ''} replace mb-12 `}>
         <div className={`${showPageCover ? 'md:w-7/12' : ''}`}>
-            <h2 className="mb-4">
-                <Link
-                    href={`/${post.slug}`}
-                    className="text-black dark:text-gray-100 text-xl md:text-2xl no-underline hover:underline">
-                    <NotionIcon icon={post.pageIcon} />{post?.title}
-                </Link>
-            </h2>
-            <time className='flex-shrink-0 text-gray-100 dark:text-gray-100'>
-              {post?.publishDay}
-            </time>
+        <h2 className="mb-4 flex items-center justify-between">
+          <Link
+            href={`/${post.slug}`}
+            className="text-black dark:text-gray-100 text-xl md:text-2xl no-underline hover:underline"
+          >
+            <NotionIcon icon={post.pageIcon} />
+            {post?.title}
+          </Link>
+          <time className='flex-shrink-0 text-gray-100 dark:text-gray-100'>
+            {post?.publishDay}
+          </time>
+        </h2>
   
              {/*<div className="mb-4 text-sm text-gray-700 dark:text-gray-300">
                by <a href="#" className="text-gray-700 dark:text-gray-300">{siteConfig('AUTHOR')}</a> on {post.date?.start_date || post.createdTime}
