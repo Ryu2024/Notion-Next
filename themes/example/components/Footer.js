9de76df741c4a9a46ff91e99db2d8caa370d9271
Ryu2024 <117675219+Ryu2024@users.noreply.github.com>
2024-05-09 16:35:44 +0800

    Update Footer.js

--- themes/example/components/Footer.js
+++ themes/example/components/Footer.js
@@ -8,7 +8,8 @@ export const Footer = (props) => {
   const copyrightDate = parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
 
   return <footer className="z-10 relative w-full bg-white px-6 border-t dark:border-hexo-black-gray dark:bg-hexo-black-gray ">
-        <DarkModeButton className='text-center pt-4'/>
+ {/*        
+<DarkModeButton className='text-center pt-4'/>
 
         <div className="container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
             <div className='text-center'> &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}. All rights reserved.</div>
@@ -21,6 +22,7 @@ export const Footer = (props) => {
                     <a href="https://github.com/tangly1024/NotionNext" className=' hover:underline'> NotionNext {siteConfig('VERSION')}  </a>
                 </span>
             </div>
-        </div>
+        </div> 
+     */}
     </footer>
 }
