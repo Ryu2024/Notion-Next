0344f1915f437bbd54bd1236dfe4645b156d067b
Ryu2024 <117675219+Ryu2024@users.noreply.github.com>
2024-05-09 12:27:01 +0800

    Update Title.js

--- themes/example_2/components/Title.js
+++ themes/example_2/components/Title.js
@@ -10,7 +10,7 @@ export const Title = (props) => {
   const { post } = props
   const title = post?.title || siteConfig('TITLE')
   const description = post?.description || siteConfig('AUTHOR')
-  return <div className="text-center px-6 py-3 mb-3 bg-gray-100 dark:bg-hexo-black-gray dark:border-hexo-black-gray border-b">
+  return <div className="text-center px-6 py-3 mb-0 bg-gray-100 dark:bg-hexo-black-gray dark:border-hexo-black-gray border-b">
         <h1 className="text-xm md:text-2xl pb-4"><NotionIcon icon={post?.pageIcon} />{title}</h1>
         {/*<p className="leading-loose text-gray-dark">
             {description}
