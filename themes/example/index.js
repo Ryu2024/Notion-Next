cd7603283acb4df0d037e4199ae38eee35ed7cb8
Ryu2024 <117675219+Ryu2024@users.noreply.github.com>
2024-05-15 22:34:00 +0800

    Update index.js

--- themes/example/index.js
+++ themes/example/index.js
@@ -312,7 +312,7 @@ const LayoutTagIndex = props => {
               key={tag}
               href={`/tag/${encodeURIComponent(tag.name)}`}
               passHref
-              className={`cursor-pointer inline-block rounded hover:bg-gray-500 hover:text-white duration-200 mr-2 py-1 px-2 text-xs whitespace-nowrap dark:hover:text-white text-gray-600 hover:shadow-xl dark:border-gray-400 notion-${tag.color}_background dark:bg-gray-800`}>
+              className={`cursor-pointer inline-block hover:bg-gray-500 hover:text-white duration-200 mr-2 py-1 px-2 text-xs whitespace-nowrap dark:hover:text-white text-gray-600 hover:shadow-xl dark:border-gray-400 notion-${tag.color}_background dark:bg-gray-800`}>
               <div className='font-light dark:text-gray-400'>
                 <i className='mr-1 fas fa-tag' />{' '}
                 {tag.name + (tag.count ? `(${tag.count})` : '')}{' '}
